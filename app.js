cbbc588c11c11c19a7d72e9f81074e4db5a509e1
Gokul Kramnik <ramakrishnan.go@husky.neu.edu>
2019-11-19 06:22:39 -0800

    Adding alerts and validations
    Adding alerts during error or success during submit. Adding validations for the input fields.

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ UI.prototype.addBook = (book) => {
         <td>${book.title}</td>
         <td>${book.author}</td>
         <td>${book.isbn}</td>
-        <td><a class='delete'>X</a></td>
+        <td><a class='delete' style="color: black;">X</a></td>
     `;
 
     list.appendChild(tableRow);
@@ -30,7 +30,27 @@ UI.prototype.clearFields = () => {
     document.getElementById('title').value = '';
     document.getElementById('author').value = '';
     document.getElementById('isbn').value = '';
-}
+};
+
+UI.prototype.showAlert = (message, className) => {
+    //construct error element
+    const div = document.createElement('div');
+    //Add classes
+    div.className = `alert ${className}`;
+    //Add Message
+    div.appendChild(document.createTextNode(message));
+    //Get parent
+    const container = document.querySelector('.container');
+    //get the form
+    const form = document.querySelector('#book-form');
+    //Insert alert before form in the container.
+    container.insertBefore(div, form);
+    //Timeout after 3 seconds
+    setTimeout(() => {
+        //Remove any alert that is shown within 3 seconds
+        document.querySelector('.alert').remove();
+    }, 3000);
+};
 
 //Event Listeners
 document.getElementById('book-form').addEventListener('submit', (event) => {
@@ -47,9 +67,18 @@ document.getElementById('book-form').addEventListener('submit', (event) => {
     //Instantiate UI
     const ui = new UI();
 
-    //Add book to list
-    ui.addBook(book);
+    //Validate the object
+    if (!isbn || !author || !title) {
 
-    ui.clearFields();
+        //Error alert
+        ui.showAlert('Please fill all the fields', 'error');
+    } else {   
+        //Add book to list
+        ui.addBook(book);
+        //Show success alert
+        ui.showAlert('Book information saved successfully', 'success');
+        //Clear Fields
+        ui.clearFields();
+    }
 
 });
